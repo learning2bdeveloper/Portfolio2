b9e2d60fec6a972d79b5984c16465c2dc671358d
Kyanu <learn.2.be.a.developer@gmail.com>
2024-10-04 07:59:55 +0800

    not sure

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -2,7 +2,7 @@ const desktop_folder_div = document.querySelectorAll(".desktop-folder-div");
 
 desktop_folder_div.forEach((folder) => {
   folder.addEventListener("click", () => {
-    let span_elem = folder.getElementsByTagName("span");
-    console.log(span_elem[0].innerText);
+    let span_elem = folder.querySelector("span");
+    console.log(span_elem.innerText);
   });
 });
